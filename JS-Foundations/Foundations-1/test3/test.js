e1bc56503a9763d037c1b6fa39ca30491e1cfea2
Litus <109820050+Litusuwu@users.noreply.github.com>
2024-07-26 10:13:36 -0500

    dasdas

---
+++ JS-Foundations/Foundations-1/test3/test.js
@@ -0,0 +1,13 @@
+const string = "Holi "
+const string2 = 'dsadas'
+console.log(string + " " + string2)
+const myInt = 5;
+const myFloat = 10.667;
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+console.log(myFloat.toPrecision(2));
+let myNumber = "80";
+myNumber += 4;
+
+console.log(myNumber);
